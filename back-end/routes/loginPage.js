50cbc7c6bd56ba38eabb7128448af48b255c3c39
AkshayaKL <8akshayakrishnan8@gmail.com>
2021-04-04 11:15:51 +0530

    Build stays

--- back-end/routes/loginPage.js
+++ back-end/routes/loginPage.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-
+router.get('/', (req,res) => 
+{res.sendFile(path.join(__dirname,'..', 'build', 'index.html'))
+    //res.render('welcomePage', {title: 'Storage Drive - Backend', titleMessage: 'Hello, Welcome to the backend!', redirectMessage: 'Please redirect to the following links', link1: 'test1', link2: 'test2'});
+});
 
 module.exports = router;
