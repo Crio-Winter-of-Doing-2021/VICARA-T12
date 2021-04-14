70530555a288743b681a855ed29b276f3833b036
RahulSenguttuvan <rahul.senguttuvan@gmail.com>
2021-04-14 12:16:07 +0530

    test

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 // Defining a base url so can be called 
 const baseURL = 'https://vicara-storage-drive.herokuapp.com/';
-//const baseURL ='http://localhost:3000/'
 const axiosInstance = axios.create({
 	baseURL: baseURL,
 		headers: {
